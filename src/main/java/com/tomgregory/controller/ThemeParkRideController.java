85fd7acb24b9223f677702bf0e8afec93be4a514
Tom Gregory <t.k.gregory@gmail.com>
2023-01-03 08:41:52 +0000

    Migrate javax.* packages to jakarta.*

--- src/main/java/com/tomgregory/controller/ThemeParkRideController.java
+++ src/main/java/com/tomgregory/controller/ThemeParkRideController.java
@@ -7,7 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 
 @RestController
 public class ThemeParkRideController {
--- src/main/java/com/tomgregory/entity/ThemeParkRide.java
+++ src/main/java/com/tomgregory/entity/ThemeParkRide.java
@@ -4,11 +4,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.NotEmpty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
 
 @Entity
 @Getter
